63dab40457418743340dc83cdd268cd414beb1ed
Jorgen812 <traasdahl@gmail.com>
2021-08-30 14:25:16 +0200

    Initial commit

---
+++ src/helloWord/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloWord;
+
+public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println("Hello Word");
+
+        System.out.println("Teller til ti:");
+
+        for (int i =1; i <=10;i++){
+            System.out.println(i);
+        }
+
+
+    }
+}
